8f000f60787ead117edbed7c3eaa77981134e96e
anatissera <atissera@udesa.edu.ar>
2024-03-22 10:39:46 -0300

    graficar en el mismo

--- punto1a.py
+++ punto1a.py
@@ -36,20 +36,41 @@ def equispaced_points(a, b, num_points):
 def chebyshev_points(a, b, num_points):
     return (a + b) / 2 + (b - a) / 2 * np.cos((2 * np.arange(1, num_points + 1) - 1) * np.pi / (2 * num_points))
 
-def graficar_interpol_ambos_puntos(f_interpol_equispaced, f_interpool_nonequispaced, x_equispaced_fa, y_equispaced_fa, x_nonequispaced_fa, y_nonequispaced_fa, method, q_points):
-    plt.figure(figsize=(12, 8))
-    plt.plot(points_to_study_function, fa(points_to_study_function), label='$f_a(x)$', linestyle='--', color='black')  # Graficar la función fa(x)
-    plt.plot(x_equispaced_fa, y_equispaced_fa, 'o', label='$Puntos de Colocación$ (Equispaciado)', color = 'blue')
-    plt.plot(points_to_study_equifunction, f_interpol_equispaced(points_to_study_equifunction), label='$Interpolación$ (Equispaciado)', color = 'green')
-    plt.plot(x_nonequispaced_fa, y_nonequispaced_fa, 'o', label='$Puntos de Colocación$ (No equiespaciado)', color = 'orange')
-    plt.plot(points_to_study_function, f_interpool_nonequispaced(points_to_study_function), label='$Interpolación$ (No equiespaciado)', color = 'red')
+def graficar_interpol_ambos_puntos(f_interpol_equispaced, f_interpol_nonequispaced, x_equispaced_fa, y_equispaced_fa, x_nonequispaced_fa, y_nonequispaced_fa, x_compare_equipoints_fa, x_compare_nonequipoints_fa, method, q_points):
+    fig, axs = plt.subplots(1, 2, figsize=(16, 6))
+    
+    points_to_study_function = equispaced_points(-3.98, 3.98, 150)
+    points_to_study_equifunction = equispaced_points(-3.46, 3.46, 150)
+    
+    # plt.figure(figsize=(12, 8))
+    
+    axs[0].plot(points_to_study_function, fa(points_to_study_function), label='$f_a(x)$', linestyle='--', color='black')  # Graficar la función fa(x)
+    axs[0].plot(x_equispaced_fa, y_equispaced_fa, 'o', label='$Puntos de Colocación$ (Equispaciado)', color = 'blue')
+    axs[0].plot(points_to_study_equifunction, f_interpol_equispaced(points_to_study_equifunction), label='$Interpolación$ (Equispaciado)', color = 'green')
+    axs[0].plot(x_nonequispaced_fa, y_nonequispaced_fa, 'o', label='$Puntos de Colocación$ (No equiespaciado)', color = 'orange')
+    axs[0].plot(points_to_study_function, f_interpol_nonequispaced(points_to_study_function), label='$Interpolación$ (No equiespaciado)', color = 'red')
+    axs[0].set_xlabel('$x$')
+    axs[0].set_ylabel('$f_a(x)$')
+    axs[0].set_title(f"Interpolación de $f_a(x)$ con {method} con {q_points} Puntos de Colocación")
+    axs[0].legend()
+    axs[0].grid(True)
+    
+    error_equispaced_w_midpoints = np.abs(fa(x_compare_equipoints_fa) - f_interpol_equispaced(x_compare_equipoints_fa))
+    error_nonequispaced_w_midpoints = np.abs(fa(x_compare_nonequipoints_fa) - f_interpol_nonequispaced(x_compare_nonequipoints_fa))
 
-    plt.xlabel('$x$')
-    plt.ylabel('$f_a(x)$')
-    plt.title(f"Interpolación de $f_a(x)$ con {method} con {q_points} Puntos de Colocación")
-    plt.legend()
-    plt.grid(True)
+    axs[1].plot(x_compare_equipoints_fa, error_equispaced_w_midpoints, 'o')
+    axs[1].plot(x_compare_nonequipoints_fa, error_nonequispaced_w_midpoints, 'o')
+    axs[1].plot(x_compare_equipoints_fa, error_equispaced_w_midpoints, label='$Error$ (Equispaciado)')
+    axs[1].plot(x_compare_nonequipoints_fa, error_nonequispaced_w_midpoints, label='$Error$ (No equiespaciado)')
+    axs[1].set_xlabel('$x$')
+    axs[1].set_ylabel('$Error$')
+    axs[1].set_title(f"Error de Interpolación con {method} de $f_a(x)$ con {q_points} puntos")
+    axs[1].legend()
+    axs[1].grid(True)
+    
+    plt.tight_layout()
     plt.show()
+    
     
 def graficar_error(f_interpol_equispaced, f_interpol_nonequispaced, x_compare_equipoints_fa, x_compare_nonequipoints_fa, method, q_points):
     error_equispaced_w_midpoints = np.abs(fa(x_compare_equipoints_fa) - f_interpol_equispaced(x_compare_equipoints_fa))
@@ -66,71 +87,66 @@ def graficar_error(f_interpol_equispaced, f_interpol_nonequispaced, x_compare_eq
     plt.legend()
     plt.grid(True)
     plt.show()
-    
-x_few_equispaced_fa = equispaced_points(xa_min, xa_max, 13) # con Lagrange es mejor menos puntos, si pongo más se arranca a disparar
-x_few_nonequispaced_fa = chebyshev_points(xa_min, xa_max, 14)
-
-x_more_equispaced_fa = equispaced_points(xa_min, xa_max, 20) 
-x_more_nonequispaced_fa = chebyshev_points(xa_min, xa_max, 20)
-
-y_few_equispaced_fa = fa(x_few_equispaced_fa)
-y_few_nonequispaced_fa = fa(x_few_nonequispaced_fa)
-
-y_more_equispaced_fa = fa(x_more_equispaced_fa)
-y_more_nonequispaced_fa = fa(x_more_nonequispaced_fa)
-
-fa_lagrange_few_equispaced = lagrange(x_few_equispaced_fa, y_few_equispaced_fa)
-fa_lagrange_few_nonequispaced = lagrange(x_few_nonequispaced_fa, y_few_nonequispaced_fa)
-
-fa_lagrange_more_equispaced = lagrange(x_more_equispaced_fa, y_more_equispaced_fa)
-fa_lagrange_more_nonequispaced = lagrange(x_more_nonequispaced_fa, y_more_nonequispaced_fa)
-
-# Graficar la interpolación de fa(x) con ambos puntos y fa(x)
-points_to_study_function = equispaced_points(-3.98, 3.98, 150)
-points_to_study_equifunction = equispaced_points(-3.46, 3.46, 150)
-
-# Graficar la interpolación de fa(x) con ambos puntos y fa(x)
-
-# buscar puntos para comparar -> para el error
 
 def generate_midpoints(lst): # -> solo los midpoints
-    midpoints = []
-    for i in range(len(lst) - 1):
-        midpoint = (lst[i] + lst[i+1]) / 2.0
-        midpoints.append(midpoint)
-    return np.array(midpoints)
+        midpoints = []
+        for i in range(len(lst) - 1):
+            midpoint = (lst[i] + lst[i+1]) / 2.0
+            midpoints.append(midpoint)
+        return np.array(midpoints)
 
-# def generate_midpoints(lst): # -> los midpoints y los puntos de interpolación
-#     midpoints = []
-#     for i in range(len(lst) - 1):
-#         midpoint = (lst[i] + lst[i+1]) / 2.0
-#         midpoints.extend([lst[i], midpoint])
-#     midpoints.append(lst[-1])  # Agregar el último elemento de la lista original
-#     return np.array(midpoints)
+    # def generate_midpoints(lst): # -> los midpoints y los puntos de interpolación
+    #     midpoints = []
+    #     for i in range(len(lst) - 1):
+    #         midpoint = (lst[i] + lst[i+1]) / 2.0
+    #         midpoints.extend([lst[i], midpoint])
+    #     midpoints.append(lst[-1])  # Agregar el último elemento de la lista original
+    #     return np.array(midpoints)
 
-x_compare_few_equipoints_fa = generate_midpoints(x_few_equispaced_fa)
-x_compare_few_nonequipoints_fa = generate_midpoints(x_few_nonequispaced_fa)
+def lagrange_interpol(num_points, method, q_points):
+            
+    x_equispaced_fa = equispaced_points(xa_min, xa_max, num_points) # con Lagrange es mejor menos puntos, si pongo más se arranca a disparar
+    x_nonequispaced_fa = chebyshev_points(xa_min, xa_max, num_points)
 
-x_compare_more_equipoints_fa = generate_midpoints(x_more_equispaced_fa)
-x_compare_more_nonequipoints_fa = generate_midpoints(x_more_nonequispaced_fa)
+    y_equispaced_fa = fa(x_equispaced_fa)
+    y_nonequispaced_fa = fa(x_nonequispaced_fa)
 
-# hacer lo mismo con splines 
-spline_few_equispaced_fa = CubicSpline(x_few_equispaced_fa, y_few_equispaced_fa)
-spline_more_equispaced_fa = CubicSpline(x_more_equispaced_fa, y_more_equispaced_fa)
+    fa_lagrange_equispaced = lagrange(x_equispaced_fa, y_equispaced_fa)
+    fa_lagrange_nonequispaced = lagrange(x_nonequispaced_fa, y_nonequispaced_fa)
 
-sorted_few_indices = np.argsort(x_few_nonequispaced_fa)
-sorted_more_indices = np.argsort(x_more_nonequispaced_fa)
-x_few_nonequispaced_fa_sorted = x_few_nonequispaced_fa[sorted_few_indices]
-y_few_nonequispaced_fa_sorted = y_few_nonequispaced_fa[sorted_few_indices]
-x_more_nonequispaced_fa_sorted = x_more_nonequispaced_fa[sorted_more_indices]
-y_more_nonequispaced_fa_sorted = y_more_nonequispaced_fa[sorted_more_indices]
+    # buscar puntos para comparar -> para el error
 
-# Interpolación con splines cúbicos
-spline_few_nonequispaced_fa = CubicSpline(x_few_nonequispaced_fa_sorted, y_few_nonequispaced_fa_sorted)
-spline_more_nonequispaced_fa = CubicSpline(x_more_nonequispaced_fa_sorted, y_more_nonequispaced_fa_sorted)
+    x_compare_equipoints_fa = generate_midpoints(x_equispaced_fa)
+    x_compare_nonequipoints_fa = generate_midpoints(x_nonequispaced_fa)
+    
+    graficar_interpol_ambos_puntos(fa_lagrange_equispaced, fa_lagrange_nonequispaced, x_equispaced_fa, y_equispaced_fa, x_nonequispaced_fa, y_nonequispaced_fa, x_compare_equipoints_fa, x_compare_nonequipoints_fa, method, q_points)
 
-points_to_study_function = equispaced_points(-3.98, 3.98, 150)
-points_to_study_equifunction = equispaced_points(-3.46, 3.46, 150)
+    # graficar_error(fa_lagrange_equispaced, fa_lagrange_nonequispaced, x_compare_equipoints_fa, x_compare_nonequipoints_fa, method, q_points)
+
+
+def splines_interpol(num_points, method, q_points):
+    # hacer lo mismo con splines 
+    x_equispaced_fa = equispaced_points(xa_min, xa_max, num_points) # con Lagrange es mejor menos puntos, si pongo más se arranca a disparar
+    x_nonequispaced_fa = chebyshev_points(xa_min, xa_max, num_points)
+
+    y_equispaced_fa = fa(x_equispaced_fa)
+    y_nonequispaced_fa = fa(x_nonequispaced_fa)
+
+    spline_equispaced_fa = CubicSpline(x_equispaced_fa, y_equispaced_fa)
+
+    sorted_few_indices = np.argsort(x_nonequispaced_fa)
+    x_nonequispaced_fa_sorted = x_nonequispaced_fa[sorted_few_indices]
+    y_nonequispaced_fa_sorted = y_nonequispaced_fa[sorted_few_indices]
+    
+    x_compare_equipoints_fa = generate_midpoints(x_equispaced_fa)
+    x_compare_nonequipoints_fa = generate_midpoints(x_nonequispaced_fa_sorted)
+
+    # Interpolación con splines cúbicos
+    spline_nonequispaced_fa = CubicSpline(x_nonequispaced_fa_sorted, y_nonequispaced_fa_sorted)
+    
+    graficar_interpol_ambos_puntos(spline_equispaced_fa, spline_nonequispaced_fa, x_equispaced_fa, y_equispaced_fa, x_nonequispaced_fa, y_nonequispaced_fa, x_compare_equipoints_fa, x_compare_nonequipoints_fa, method, q_points)
+
+    # graficar_error(spline_equispaced_fa, spline_nonequispaced_fa, x_compare_equipoints_fa, x_compare_nonequipoints_fa, method, q_points)
 
 # plt.plot(x_compare_equipoints_fa, y_compare_equipoints_fa, 'o', label='$Puntos de Colocación$ (Equispaciado)')
 # plt.plot(x_compare_nonequipoints_fa, y_nonequispaced_fa, 'o', label='$Puntos de Colocación$ (No equiespaciado)')
@@ -188,23 +204,10 @@ points_to_study_equifunction = equispaced_points(-3.46, 3.46, 150)
 # min_error_nonequispaced = np.min(error_nonequispaced)
 
 def graficar():
-    
-    graficar_interpol_ambos_puntos(fa_lagrange_few_equispaced, fa_lagrange_few_nonequispaced, x_few_equispaced_fa, y_few_equispaced_fa, x_few_nonequispaced_fa, y_few_nonequispaced_fa, "Lagrange", "pocos")
-
-    graficar_error(fa_lagrange_few_equispaced, fa_lagrange_few_nonequispaced, x_compare_few_equipoints_fa, x_compare_few_nonequipoints_fa, "Lagrange", "pocos")
-
-    graficar_interpol_ambos_puntos(fa_lagrange_more_equispaced, fa_lagrange_more_nonequispaced, x_more_equispaced_fa, y_more_equispaced_fa, x_more_nonequispaced_fa, y_more_nonequispaced_fa, "Lagrange", "más")
-
-    graficar_error(fa_lagrange_more_equispaced, fa_lagrange_more_nonequispaced, x_compare_more_equipoints_fa, x_compare_more_nonequipoints_fa, "Lagrange", "más")
-
-    # Graficar la interpolación de fa(x) con ambos puntos y fa(x)
-    graficar_interpol_ambos_puntos(spline_few_equispaced_fa, spline_few_nonequispaced_fa, x_few_equispaced_fa, y_few_equispaced_fa, x_few_nonequispaced_fa, y_few_nonequispaced_fa, "Splines Cúbicos", "pocos")
-
-    graficar_error(spline_few_equispaced_fa, spline_few_nonequispaced_fa, x_compare_few_equipoints_fa, x_compare_few_nonequipoints_fa, "Splines Cúbicos", "pocos")
-
-    graficar_interpol_ambos_puntos(spline_more_equispaced_fa, spline_more_nonequispaced_fa, x_more_equispaced_fa, y_more_equispaced_fa, x_more_nonequispaced_fa, y_more_nonequispaced_fa, "Splines Cúbicos", "más")
-
-    graficar_error(spline_more_equispaced_fa, spline_more_nonequispaced_fa, x_compare_more_equipoints_fa, x_compare_more_nonequipoints_fa, "Splines Cúbicos", "más")
+    lagrange_interpol(13, "Lagrange", "pocos")
+    lagrange_interpol(20, "Lagrange", "muchos")
+    splines_interpol(13, "Splines", "pocos")
+    splines_interpol(20, "Splines", "muchos")
 
 def main():
     graficar()
